a5f3fdb30731a40d055f0905387baa2335b1782a
MAP34348 <MAP34348@SOFT270>
2018-10-11 12:26:56 -0300

    Quitar Carrito Completo

--- Presentacion/Controllers/ProductoController.cs
+++ Presentacion/Controllers/ProductoController.cs
@@ -19,8 +19,15 @@ namespace Presentacion.Controllers
         }
 
 
-        public ActionResult DetalleProductoMC110()
+        public ActionResult DetalleProducto(int productoId = 0)//int codProducto = 0)
         {
+            ViewBag.Imagen = "../../Imagenes/Fotos Maquinas/1.Manuales/MT-110.jpg";
+            ViewBag.Titulo = "Impresora Manual";
+            ViewBag.Modelo = "MC-110";
+            ViewBag.Descripcion = "Máquina manual para imprimir en telas. Máxima área de impresión 150x100mm.";
+            ViewBag.Precio = "23.200";
+            ViewBag.CodigoProducto = productoId;
+
             return View();
         }
 
@@ -66,7 +73,33 @@ namespace Presentacion.Controllers
                 Session["Carrito"] = productosCarrito;
             }
 
+            Session["ItemsCarrito"] = ((int)Session["ItemsCarrito"] + 1);
+
             return View();
+        }
+
+        public ActionResult QuitarProductoCarrito(int productoId)
+        {
+            List<Carrito> productosCarrito = (List<Carrito>)Session["Carrito"];
+
+            int idProdCarrito = controlarId(productoId);
+
+            productosCarrito[idProdCarrito].Cantidad--;
+
+            if (productosCarrito[idProdCarrito].Cantidad == 0)
+
+            { productosCarrito.RemoveAt(idProdCarrito); }
+
+            Session["Carrito"] = productosCarrito;
+
+            int cantidadTotalItems = (int)Session["ItemsCarrito"];
+
+            Session["ItemsCarrito"] = ((int)Session["ItemsCarrito"] - 1);
+
+            cantidadTotalItems = (int)Session["ItemsCarrito"];
+
+            return View("MostrarCarrito");
+
         }
 
         private int controlarId(int id)
@@ -81,6 +114,37 @@ namespace Presentacion.Controllers
 
             return -1;
         }
+
+        private int verCantidadProductos(int id)
+        {
+            List<Carrito> productosCarrito = (List<Carrito>)Session["Carrito"];
+
+            for (int i = 0; i < productosCarrito.Count; i++)
+            {
+                if (productosCarrito[i].ProductoId == id)
+
+                    return productosCarrito[i].Cantidad;
+            }
+
+            return -1;
+        }
+
+        private int restarProducto(int id)
+        {
+            List<Carrito> productosCarrito = (List<Carrito>)Session["Carrito"];
+
+            for (int i = 0; i < productosCarrito.Count; i++)
+            {
+                if (productosCarrito[i].ProductoId == id)
+
+                    return (productosCarrito[i].Cantidad--);
+
+            }
+
+            return -1;
+
+        }
+
     }
 }
 
