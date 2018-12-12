6517e9ae46a822b19848079532161fc4d318be54
pabloam12 <pablo.a.mahiques@gmail.com>
2018-12-12 11:36:37 -0300

    Exportadores con try catch.

--- Servicios/Exportador.cs
+++ Servicios/Exportador.cs
@@ -13,34 +13,40 @@ namespace Servicios
 
         public void ExportarBitacoraXML(List<Bitacora> listaBitacora)
         {
-
-            var fechaHora = DateTime.Now.ToShortDateString();
-            var dia = fechaHora.Substring(0, 2);
-            var mes = fechaHora.Substring(3, 2);
-            var anio = fechaHora.Substring(6, 4);
-
-            using (XmlWriter writer = XmlWriter.Create("C:\\Implantagraf\\XML\\Bitacora_" + anio + mes + dia + ".xml"))
+            try
             {
-                writer.WriteStartDocument();
-                writer.WriteStartElement("BitacoraXML");
+                var fechaHora = DateTime.Now.ToShortDateString();
+                var dia = fechaHora.Substring(0, 2);
+                var mes = fechaHora.Substring(3, 2);
+                var anio = fechaHora.Substring(6, 4);
 
-                foreach (Bitacora bitacoraActual in listaBitacora)
+                using (XmlWriter writer = XmlWriter.Create("C:\\Implantagraf\\XML\\Bitacora_" + anio + mes + dia + ".xml"))
                 {
-                    writer.WriteStartElement("Bitacora");
+                    writer.WriteStartDocument();
+                    writer.WriteStartElement("BitacoraXML");
 
-                    writer.WriteAttributeString("Id", bitacoraActual.Id.ToString());
+                    foreach (Bitacora bitacoraActual in listaBitacora)
+                    {
+                        writer.WriteStartElement("Bitacora");
 
-                    writer.WriteElementString("FechaHora", bitacoraActual.FechaHora.ToString());
-                    writer.WriteElementString("Usuario", bitacoraActual.Usuario);
-                    writer.WriteElementString("Accion", bitacoraActual.Accion);
-                    writer.WriteElementString("Criticidad", bitacoraActual.Criticidad);
-                    writer.WriteElementString("Detalle", bitacoraActual.Detalle);
+                        writer.WriteAttributeString("Id", bitacoraActual.Id.ToString());
+
+                        writer.WriteElementString("FechaHora", bitacoraActual.FechaHora.ToString());
+                        writer.WriteElementString("Usuario", bitacoraActual.Usuario);
+                        writer.WriteElementString("Accion", bitacoraActual.Accion);
+                        writer.WriteElementString("Criticidad", bitacoraActual.Criticidad);
+                        writer.WriteElementString("Detalle", bitacoraActual.Detalle);
+
+                        writer.WriteEndElement();
+                    }
 
                     writer.WriteEndElement();
+                    writer.WriteEndDocument();
                 }
+            }
+            catch
+            {
 
-                writer.WriteEndElement();
-                writer.WriteEndDocument();
             }
 
         }
@@ -48,72 +54,80 @@ namespace Servicios
 
         public void ExportarClientesXML(List<Usuario> listaClientes)
         {
-            var fechaHora = DateTime.Now.ToShortDateString();
-            var dia = fechaHora.Substring(0, 2);
-            var mes = fechaHora.Substring(3, 2);
-            var anio = fechaHora.Substring(6, 4);
-
-            using (XmlWriter writer = XmlWriter.Create("C:\\Implantagraf\\XML\\Clientes_" + anio + mes + dia + ".xml"))
+            try
             {
-                writer.WriteStartDocument();
-                writer.WriteStartElement("ClientesXML");
+                var fechaHora = DateTime.Now.ToShortDateString();
+                var dia = fechaHora.Substring(0, 2);
+                var mes = fechaHora.Substring(3, 2);
+                var anio = fechaHora.Substring(6, 4);
 
-                foreach (Usuario clienteActual in listaClientes)
+                using (XmlWriter writer = XmlWriter.Create("C:\\Implantagraf\\XML\\Clientes_" + anio + mes + dia + ".xml"))
                 {
-                    writer.WriteStartElement("Cliente");
+                    writer.WriteStartDocument();
+                    writer.WriteStartElement("ClientesXML");
 
-                    writer.WriteAttributeString("Id", clienteActual.Id.ToString());
+                    foreach (Usuario clienteActual in listaClientes)
+                    {
+                        writer.WriteStartElement("Cliente");
 
-                    writer.WriteElementString("FechaAlta", clienteActual.FechaAlta.ToString());
-                    writer.WriteElementString("RazonSocial", clienteActual.RazonSocial);
-                    writer.WriteElementString("CUIL", clienteActual.CUIL);
-                    writer.WriteElementString("Email", clienteActual.Email);
-                    writer.WriteElementString("Telefono", clienteActual.Telefono);
-                    writer.WriteElementString("Direccion", clienteActual.Direccion);
-                    writer.WriteElementString("Descripcion", clienteActual.Localidad.Descripcion);
-                    
+                        writer.WriteAttributeString("Id", clienteActual.Id.ToString());
+
+                        writer.WriteElementString("FechaAlta", clienteActual.FechaAlta.ToString());
+                        writer.WriteElementString("RazonSocial", clienteActual.RazonSocial);
+                        writer.WriteElementString("CUIL", clienteActual.CUIL);
+                        writer.WriteElementString("Email", clienteActual.Email);
+                        writer.WriteElementString("Telefono", clienteActual.Telefono);
+                        writer.WriteElementString("Direccion", clienteActual.Direccion);
+                        writer.WriteElementString("Descripcion", clienteActual.Localidad.Descripcion);
+
+                        writer.WriteEndElement();
+                    }
+
                     writer.WriteEndElement();
+                    writer.WriteEndDocument();
                 }
-
-                writer.WriteEndElement();
-                writer.WriteEndDocument();
             }
-
+            catch { }
         }
 
 
         public void ExportarVentasXML(List<Operacion> listaVentas)
         {
-            var fechaHora = DateTime.Now.ToShortDateString();
-            var dia = fechaHora.Substring(0, 2);
-            var mes = fechaHora.Substring(3, 2);
-            var anio = fechaHora.Substring(6, 4);
-
-            using (XmlWriter writer = XmlWriter.Create("C:\\Implantagraf\\XML\\Ventas_" + anio + mes + dia + ".xml"))
+            try
             {
-                writer.WriteStartDocument();
-                writer.WriteStartElement("VentasXML");
+                var fechaHora = DateTime.Now.ToShortDateString();
+                var dia = fechaHora.Substring(0, 2);
+                var mes = fechaHora.Substring(3, 2);
+                var anio = fechaHora.Substring(6, 4);
 
-                foreach (Operacion ventaActual in listaVentas)
+                using (XmlWriter writer = XmlWriter.Create("C:\\Implantagraf\\XML\\Ventas_" + anio + mes + dia + ".xml"))
                 {
-                    writer.WriteStartElement("Venta");
+                    writer.WriteStartDocument();
+                    writer.WriteStartElement("VentasXML");
 
-                    writer.WriteAttributeString("Codigo", ventaActual.Id.ToString());
+                    foreach (Operacion ventaActual in listaVentas)
+                    {
+                        writer.WriteStartElement("Venta");
 
-                    writer.WriteElementString("FechaHora", ventaActual.FechaHora.ToString());
-                    writer.WriteElementString("Cliente", ventaActual.Cliente.RazonSocial);                    
-                    writer.WriteElementString("Importe", ventaActual.ImporteTotal.ToString());
-                    writer.WriteElementString("Estado", ventaActual.Estado.Descripcion);
-                    writer.WriteElementString("FormaPago", ventaActual.FormaPago.Descripcion);
-                    writer.WriteElementString("Factura", ventaActual.Factura.Codigo.ToString());
+                        writer.WriteAttributeString("Codigo", ventaActual.Id.ToString());
+
+                        writer.WriteElementString("FechaHora", ventaActual.FechaHora.ToString());
+                        writer.WriteElementString("Cliente", ventaActual.Cliente.RazonSocial);
+                        writer.WriteElementString("Importe", ventaActual.ImporteTotal.ToString());
+                        writer.WriteElementString("Estado", ventaActual.Estado.Descripcion);
+                        writer.WriteElementString("FormaPago", ventaActual.FormaPago.Descripcion);
+                        writer.WriteElementString("Factura", ventaActual.Factura.Codigo.ToString());
+
+                        writer.WriteEndElement();
+                    }
 
                     writer.WriteEndElement();
+                    writer.WriteEndDocument();
                 }
 
-                writer.WriteEndElement();
-                writer.WriteEndDocument();
-            }
 
+            }
+            catch { }
         }
 
 
